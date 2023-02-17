1e7c3930375ec7020e06476ce0f1bdf438e1925b
Maxi <maxigonnet17@gmail.com>
2023-02-17 20:58:53 -0300

    cuarta subida

---
+++ js/glider.js
@@ -0,0 +1,32 @@
+/*window.addEventListener('load', function(){
+    new Glider(document.querySelector('.carousel__list'), {
+       // Mobile-first defaults
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  dots: '.carousel__indicators',
+  arrows: {
+    prev: '.carousel__prev',
+    next: '.carousel__next'
+  },
+  responsive: [
+    {
+      // screens greater than >= 775px
+      breakpoint: 775,
+      settings: {
+        // Set to `auto` and provide item width to adjust to viewport
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    },{
+      // screens greater than >= 1024px
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    }
+  ]
+    })
+  })
+*/
+  
